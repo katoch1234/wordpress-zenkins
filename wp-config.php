183039ba5790f72b83950f3632eb49a5cf3cb596
katoch1234 <katochvaibhav3@gmail.com>
2024-07-30 08:44:19 +0000

    test

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', getenv('DB_NAME') );
+define( 'DB_NAME', $_ENV['DB_NAME'] );
 
 /** Database username */
 define( 'DB_USER', getenv('DB_USER'));
