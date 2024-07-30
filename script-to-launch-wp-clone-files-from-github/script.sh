3e8a1d5052804d57e9d78e8654ea28b1a9d243c9
katoch1234 <katochvaibhav3@gmail.com>
2024-07-30 08:37:58 +0000

    Added .env file

--- script-to-launch-wp-clone-files-from-github/script.sh
+++ script-to-launch-wp-clone-files-from-github/script.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-apt update;
-apt install nginx php8.1 php8.1-fpm libapache2-mod-php php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip git -y;
-systemctl stop apache2 && apt purge apache2 -y;
-systemctl start nginx;
-systemctl start php8.1-fpm;
+sudo apt update;
+sudo apt install nginx php8.1 php8.1-fpm libapache2-mod-php php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip git -y;
+sudo systemctl stop apache2 && sudo apt purge apache2 -y;
+sudo systemctl start nginx;
+sudo systemctl start php8.1-fpm;
 cd /tmp/ && git clone https://github.com/katoch1234/wordpress-zenkins.git;
-mv wordpress-zenkins/nginx-conf/default /etc/nginx/sites-enabled/;
-mv wordpress-zenkins/* /var/www/html/;
-rm -r wordpress-zenkins;
-SetEnv DB_NAME wordpress;
-SetEnv DB_USER wordpress;
-SetEnv DB_PASSWORD HappyDay$2024;
-SetEnv DB_HOST 35.193.105.139;
+sudo mv wordpress-zenkins/nginx-conf/default /etc/nginx/sites-enabled/;
+#sudo mv wordpress-zenkins/* /var/www/html/;
+sudo rsync -av --progress wordpress-zenkins/ /var/www/html/
+sudo chown -R www-data:www-data /var/www/html/ && sudo chmod 700 /var/www/html/.env
+sudo rm -r wordpress-zenkins;
