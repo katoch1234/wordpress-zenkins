0a8515b23c8e0fe70cfdf7ad20483a7caf19805a
cu.16bcs1092 <cu.16bcs1092@gmail.com>
2024-07-31 10:13:21 +0530

    added git user-name in script

--- script-to-launch-wp-clone-files-from-github/script.sh
+++ script-to-launch-wp-clone-files-from-github/script.sh
@@ -4,6 +4,8 @@ sudo apt install nginx php8.1 php8.1-fpm libapache2-mod-php php-mysql php-curl p
 sudo systemctl stop apache2 && sudo apt purge apache2 -y;
 sudo systemctl start nginx;
 sudo systemctl start php8.1-fpm;
+git config --global user.name "katoch1234"
+git config --global user.email "katochvaibhav3@gmail.com"
 cd /tmp/ && git clone https://github.com/katoch1234/wordpress-zenkins.git;
 sudo mv wordpress-zenkins/nginx-conf/default /etc/nginx/sites-enabled/;
 #sudo mv wordpress-zenkins/* /var/www/html/;
